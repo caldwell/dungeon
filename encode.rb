6752adfd0d02e655b97bb9b02c008227e56dd025
David Caldwell <david@porkrind.org>
2022-04-25 21:38:41 -0700

    encode.rb: Group encoding function + vars + serialization into a class

--- encode.rb
+++ encode.rb
@@ -9,20 +9,43 @@ end).parse!
 
 input = ARGV.shift
 
-encrypt_key='david+caldwell@porkrind.org'
+class BinaryEncoder # Built to match the decoder in dsub.c
+  attr_accessor :index, :data
 
-$encrypt=encrypt_key.split('').map {|c| c.ord}
-$index=[]
-$data=[0] # invalid entry @ zero, so all valid entries are !0
+  @@encrypt_key='david+caldwell@porkrind.org'
 
+  def initialize
+    @encrypt=@@encrypt_key.split('').map {|c| c.ord}
+    @index=[]
+    @data=[0] # invalid entry @ zero, so all valid entries are !0
+  end
 
-def add(id, text)
-  x = $index[-id] = $data.count
-  $data.append(*(text.chomp('').split('')+[0]).map {|c|
-                 enc = c.ord ^ $encrypt[x % $encrypt.count] ^ (x & 0xff)
-                 x+=1
-                 enc
-               })
+  def add(id, text)
+    x = @index[-id] = @data.count
+    @data.append(*(text.chomp('').split('')+[0]).map {|c|
+                   enc = c.ord ^ @encrypt[x % @encrypt.count] ^ (x & 0xff)
+                   x+=1
+                   enc
+                 })
+  end
+
+  def index_to_s
+    "int dtext_index[] = {\n" +
+      @index.each_with_index.map {|off,i| "    [#{i}] = #{off},\n" if off }.join("") +
+    "};\n"
+  end
+
+  def data_to_s
+    "unsigned char dtext_data[] = {\n" +
+      @data.each_slice(16).map { |chunk|
+        "    "+ chunk.map {|c| sprintf("0x%02x,",c) }.join(" ") + "\n"
+      }.join("") +
+    "};\n"
+  end
+
+  def to_s
+    index_to_s + data_to_s
+  end
 end
 
 require 'pp'
@@ -43,14 +66,8 @@ raw[id]=text
 
 raw.each {|id, text| add(id, text) }
 
-puts "int dtext_index[] = {"
-$index.each_with_index {|off,i| puts "    [#{i}] = #{off}," if off }
-puts "};"
+encoder = BinaryEncoder.new()
 
-puts "unsigned char dtext_data[] = {"
-$data.each_slice(16).each { |chunk|
-  puts "    "+
-       chunk.map {|c| sprintf("0x%02x,",c) }
-         .join(" ")
-}
-puts "};"
+raw.each {|id, text| encoder.add(id, text) }
+
+puts encoder.to_s
